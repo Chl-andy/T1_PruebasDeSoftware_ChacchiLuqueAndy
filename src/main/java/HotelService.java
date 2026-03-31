a76bc6919f208c70ab0538427dc279ae3eb3791e
Chl_andy <realforever.info@gmail.com>
2026-03-30 22:38:39 -0500

    Segundo commit - Test 1: Campos vacios

--- src/main/java/HotelService.java
+++ src/main/java/HotelService.java
@@ -1,3 +1,25 @@
+import java.time.LocalDate;
+
 public class HotelService {
-    
+
+    public String registrarHabitacion(String numero, String nombre, LocalDate fecha) {
+
+        if (numero.isEmpty() || nombre.isEmpty() || fecha == null) {
+            return "Debe ingresar los datos requeridos";
+        }
+
+        if (!numero.matches("[1-3][0-9]{2}")) {
+            return "Ingrese una habitación valida";
+        }
+
+        if (!nombre.matches("[a-zA-Z]{4,}")) {
+            return "Recuerde que el nombre del cliente debe contener al menos cuatro caracteres";
+        }
+
+        if (!fecha.isAfter(LocalDate.now())) {
+            return "Debe ingresar una fecha valida";
+        }
+
+        return "El registro ha sido exitoso";
+    }
 }
--- src/test/java/HotelServiceTest.java
+++ src/test/java/HotelServiceTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotelServiceTest {
+
+    @Test
+    void testCamposVacios() {
+        HotelService service = new HotelService();
+
+        String resultado = service.registrarHabitacion("", "", null);
+        System.out.println(resultado);
+
+        assertEquals("Debe ingresar los datos requeridos", resultado);
+    }
 
 }
