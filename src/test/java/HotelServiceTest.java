26bae5f56e2fc34ad655dbe530fbf83c59ef908b
Chl_andy <realforever.info@gmail.com>
2026-03-30 23:22:30 -0500

    Octavo commit - Test 3: "Validacion del nombre del cliente"

--- src/test/java/HotelServiceTest.java
+++ src/test/java/HotelServiceTest.java
@@ -9,8 +9,10 @@ class HotelServiceTest {
     @Test
     void testNombreClienteInvalido() {
         HotelService service = new HotelService();
+
         String resultado = service.registrarHabitacion("101", "Ana", LocalDate.now().plusDays(1));
 
+        System.out.println("Resultado: " + resultado);
         assertEquals("Recuerde que el nombre del cliente debe contener al menos cuatro caracteres", resultado);
     }
 }
